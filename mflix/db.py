f488b95acd770765f7408e1c311355b694af0795
Vladlen Kuzminov <ooyshit@gmail.com>
2019-02-26 13:45:29 +0200

    chapter2: User-Facing Backend

--- mflix/db.py
+++ mflix/db.py
@@ -151,7 +151,7 @@ def get_movies_faceted(filters, page, movies_per_page):
 
     # TODO: Faceted Search
     # Add the necessary stages to the pipeline variable in the correct order.
-    pipeline = []
+    pipeline.extend([skip_stage, limit_stage, facet_stage])
 
     try:
         movies = list(db.movies.aggregate(pipeline, allowDiskUse=True))[0]
@@ -234,7 +234,7 @@ def get_movies(filters, page, movies_per_page):
 
     # TODO: Paging
     # Use the cursor to only return the movies that belong on the current page.
-    movies = cursor.limit(movies_per_page)
+    movies = cursor.skip(page * movies_per_page).limit(movies_per_page)
 
     return (list(movies), total_num_movies)
 
@@ -264,15 +264,32 @@ def get_movie(id):
                 "$match": {
                     "_id": ObjectId(id)
                 }
-            }
+            },
+            {
+                "$lookup": {
+                    'from': 'comments',
+                    'let': {'id': '$_id'},
+                    'pipeline': [
+                        {
+                            '$match': {
+                                '$expr': {'$eq': ['$movie_id', '$$id']}
+                            },
+                        },
+                        {
+                            '$sort': {
+                                'date': -1,
+                            },
+                        },
+                    ],
+                    'as': 'comments'
+                }
+            },
         ]
-
-        movie = db.movies.aggregate(pipeline).next()
-        return movie
+        return db.movies.aggregate(pipeline).next()
 
     # TODO: Error Handling
     # If an invalid ID is passed to `get_movie`, it should return None.
-    except (StopIteration) as _:
+    except InvalidId:
 
         """
         Ticket: Error Handling
@@ -325,7 +342,13 @@ def add_comment(movie_id, user, comment, date):
     """
     # TODO: Create/Update Comments
     # Construct the comment document to be inserted into MongoDB.
-    comment_doc = { "some_field": "some_value" }
+    comment_doc = {
+        "name": user.name,
+        "email": user.email,
+        "movie_id": ObjectId(movie_id),
+        "text": comment,
+        "date": date,
+    }
     return db.comments.insert_one(comment_doc)
 
 
@@ -339,8 +362,16 @@ def update_comment(comment_id, user_email, text, date):
     # Use the user_email and comment_id to select the proper comment, then
     # update the "text" and "date" of the selected comment.
     response = db.comments.update_one(
-        { "some_field": "some_value" },
-        { "$set": { "some_other_field": "some_other_value" } }
+        {
+            "email": user_email,
+            "_id": ObjectId(comment_id),
+        },
+        {
+            "$set": {
+                "text": text,
+                "date": date,
+            },
+        },
     )
 
     return response
@@ -361,7 +392,10 @@ def delete_comment(comment_id, user_email):
 
     # TODO: Delete Comments
     # Use the user_email and comment_id to delete the proper comment.
-    response = db.comments.delete_one( { "_id": ObjectId(comment_id) } )
+    response = db.comments.delete_one({
+        "_id": ObjectId(comment_id),
+        "email": user_email,
+    })
     return response
 
 
@@ -388,7 +422,7 @@ def get_user(email):
     """
     # TODO: User Management
     # Retrieve the user document corresponding with the user's email.
-    return db.users.find_one({ "some_field": "some_value" })
+    return db.users.find_one({ "email": email })
 
 
 def add_user(name, email, hashedpw):
@@ -410,9 +444,9 @@ def add_user(name, email, hashedpw):
         # TODO: Durable Writes
         # Use a more durable Write Concern for this operation.
         db.users.insert_one({
-            "name": "mongo",
-            "email": "mongo@mongodb.com",
-            "password": "flibbertypazzle"
+            "name": name,
+            "email": email,
+            "password": hashedpw,
         })
         return {"success": True}
     except DuplicateKeyError:
@@ -431,8 +465,9 @@ def login_user(email, jwt):
         # Use an UPSERT statement to update the "jwt" field in the document,
         # matching the "user_id" field with the email passed to this function.
         db.sessions.update_one(
-            { "some_field": "some_value" },
-            { "$set": { "some_other_field": "some_other_value" } }
+            { "user_id": email },
+            { "$set": { "jwt": jwt } },
+            upsert=True,
         )
         return {"success": True}
     except Exception as e:
@@ -449,7 +484,7 @@ def logout_user(email):
     try:
         # TODO: User Management
         # Delete the document in the `sessions` collection matching the email.
-        db.sessions.delete_one({ "some_field": "some_value" })
+        db.sessions.delete_one({ "user_id": email })
         return {"success": True}
     except Exception as e:
         return {"error": e}
@@ -464,7 +499,7 @@ def get_user_session(email):
     try:
         # TODO: User Management
         # Retrieve the session document corresponding with the user's email.
-        return db.sessions.find_one({ "some_field": "some_value" })
+        return db.sessions.find_one({ "user_id": email })
     except Exception as e:
         return {"error": e}
 
@@ -477,8 +512,8 @@ def delete_user(email):
     try:
         # TODO: User Management
         # Delete the corresponding documents from `users` and `sessions`.
-        db.sessions.delete_one({ "some_field": "some_value" })
-        db.users.delete_one({ "some_field": "some_value" })
+        db.sessions.delete_one({ "user_id": email })
+        db.users.delete_one({ "email": email })
         if get_user(email) is None:
             return {"success": True}
         else:
@@ -505,8 +540,8 @@ def update_prefs(email, prefs):
         # TODO: User preferences
         # Use the data in "prefs" to update the user's preferences.
         response = db.users.update_one(
-            { "some_field": "some_value" },
-            { "$set": { "some_other_field": "some_other_value" } }
+            { "email": email },
+            { "$set": { "preferences": prefs } }
         )
         if response.matched_count == 0:
             return {'error': 'no user found'}
